7fe34d6a953f87c863356778d1b15178b00c8f3b
juliajemals <juel.com@gmail.com>
2019-10-01 12:50:10 +0200

    feat: Exploring reddit data

---
+++ exploring_reddit.R
@@ -0,0 +1,13 @@
+reddit <- read.csv('reddit.csv')
+
+table(reddit$employment.status)
+
+str(reddit)
+levels(reddit$age.range)
+
+reddit$age.range <- factor(reddit$age.range, levels = c('Under 18', '18-24', '25-34', 
+           '35-44', '45-54', '55-64', '65 or Above'),
+            ordered = T)
+
+library(ggplot2)
+qplot(data =  reddit, x = age.range)
