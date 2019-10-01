695f4a548e0b704394f0b324ac5e4c984a4fc993
juliajemals <juel.com@gmail.com>
2019-10-01 12:52:55 +0200

    docs: Add comments to file

--- exploring_reddit.R
+++ exploring_reddit.R
@@ -1,13 +1,20 @@
+# reading in the csv file
 reddit <- read.csv('reddit.csv')
 
+# getting employment status and values per result
 table(reddit$employment.status)
 
+# looking at all columns in table
 str(reddit)
+
+# looking at all age ranges in the age range variable
 levels(reddit$age.range)
 
+# reordering the age ranges as defined in levels
 reddit$age.range <- factor(reddit$age.range, levels = c('Under 18', '18-24', '25-34', 
            '35-44', '45-54', '55-64', '65 or Above'),
             ordered = T)
 
+# plotting the age ranges
 library(ggplot2)
 qplot(data =  reddit, x = age.range)
